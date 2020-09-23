655d1c9b6f2ff689319f2362e932cc6702db5e11
hendra-agil <agilsyaputra26@gmail.com>
2020-09-23 21:57:51 +0700

    Modal animation

---
+++ src/components/Modal.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const backdropVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+const modalVariants = {
+  hidden: {
+    y: '-100vh',
+    opacity: 0,
+  },
+  visible: {
+    y: '200px',
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    backgroundColor: '#444',
+    color: '#fff',
+    scale: 0.9,
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
+const Modal = ({ showModal, setShowModal }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdropVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          <motion.div className="modal" variants={modalVariants}>
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <motion.button variants={buttonVariants} whileHover="hover">
+                Start Again
+              </motion.button>
+            </Link>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Modal;
