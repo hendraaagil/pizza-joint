ccdc81455c2fd09db506f96fb1109e8804e09a87
hendra-agil <agilsyaputra26@gmail.com>
2020-09-23 18:26:28 +0700

    Initial animation state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   return (
@@ -16,9 +17,9 @@ const Header = () => {
           <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
         </svg>
       </div>
-      <div className="title">
+      <motion.div className="title" initial={{ y: -250 }} animate={{ y: -10 }}>
         <h1>Pizza Joint</h1>
-      </div>
+      </motion.div>
     </header>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
       <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <button>Create Your Pizza</button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
